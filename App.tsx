69b393020132a44cdc9aacf59141dbadf2921018
BeneKenobi <mail@planetbene.de>
2021-11-30 20:53:35 +0100

    feat: ✨ add initial location retrieval

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import * as Location from 'expo-location';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.text}>take me home</Text>
-      <StatusBar style="auto" />
-    </View>
+      {GetPosition()}
+      <StatusBar style="light" hidden={false} />
+    </SafeAreaView>
   );
 }
 
-// this is a comment in order to test the pull request feature
+const GetPosition = () => {
+
+  const [resultText, setResultText] = useState('Waiting for permission...');
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setResultText('Permission to access location was denied. Please change your settings manually to use this app.');
+      } else {
+        setResultText('Getting your location...');
+        (async () => {
+          let location = await Location.getCurrentPositionAsync({});
+          setResultText(JSON.stringify(location));
+        })();
+      }
+    })();
+  }, []);
+
+  return (
+    <Text style={styles.debug}>{resultText}</Text>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -23,5 +47,11 @@ const styles = StyleSheet.create({
   text: {
     color: 'white',
     fontSize: 100
+  },
+  debug: {
+    color: 'white',
+    fontSize: 10
   }
 });
+
+export default App;
